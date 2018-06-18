720dbe477c9636fc16b3e006b82273228210c75d
piyush gupta <piyushgupta725@gmail.com>
2018-06-18 22:17:23 +0530

    class 8 completed

--- Class 8/Substring.java
+++ Class 8/Substring.java
@@ -1,7 +1,7 @@
  import java.util.Scanner;
  class Substring{
   public static void main(String[] args){
-	  int s ,length,i,j;
+	  int length,i,j;
 	Scanner sc = new Scanner(System.in);
 	System.out.println("enter the string");
 	String str = sc.next();
@@ -9,7 +9,7 @@
    
 	for( i=0;i<length;i++)
 	{for( j=1;j<=length-i;j++)
-		{ s= str.substringln(i, i+j);
+		{ String s= str.substring(i, i+j);
 	     System.out.println(s);
 	}
   }
